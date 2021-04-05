756d03e23c6e21058a47d9e12108fd08dbc9fc22
ananliangliang <ananliangliang@vip.qq.com>
2021-04-05 22:09:45 +0800

    change to spring data JPA

---
+++ src/main/java/pers/ananliangliang/wiki/entity/EBook.java
@@ -0,0 +1,41 @@
+package pers.ananliangliang.wiki.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/** 电子书 */
+@Data
+@Entity
+public class EBook {
+    /** id */
+    private @Id @GeneratedValue Long id;
+
+    /** 名称 */
+    private @Column(length = 63) String name;
+
+    /** 分类1 */
+    private @Column(name = "category1_id") Long category1Id;
+
+    /** 分类2 */
+    private @Column(name = "category2_id") Long category2Id;
+
+    /** 描述 */
+    private String description;
+
+    /** 封面 */
+    private String cover;
+
+    /** 文档数 */
+    private Integer docCount;
+
+    /** 封面数 */
+    private Integer viewCount;
+
+    /** 点赞数 */
+    private Integer voteCount;
+
+}
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists test
-(
-    id   bigint      not null comment 'id' primary key,
-    name varchar(50) comment '名称',
-    password varchar(50) comment '密码'
-)
-    comment '测试';
-
